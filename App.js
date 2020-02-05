7763cc9f5b95125a8a2bb2002c721c987dca5f8e
Ahmed AbuQamar <devqmr@gmail.com>
2020-02-05 17:46:53 +0200

    Add Header

--- App.js
+++ App.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './components/Header';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View>
+      <Header title="Guess Number Game" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
 });
